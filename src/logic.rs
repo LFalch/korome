5bf4199c5c6ac4e6f21d70f9915ffc9beab430bf
LFalch <lucas@wasd.dk>
2016-01-22 23:04:58 +0100

    Add mouse events to `FrameInfo`

--- src/logic.rs
+++ src/logic.rs
@@ -8,7 +8,7 @@ use time::precise_time_s as time_s;
 
 use glium::glutin::{Event, ElementState};
 
-pub use glium::glutin::VirtualKeyCode;
+pub use glium::glutin::{VirtualKeyCode, MouseButton};
 
 /// Manages events and frames
 pub struct GameManager<'a>{
@@ -33,6 +33,7 @@ impl<'a> GameManager<'a>{
     /// Returns some tuple of a `FrameInfo` and a ´Drawer` or None if the window has been closed
     pub fn next_frame(&mut self) -> Option<(FrameInfo, Drawer)>{
         let mut keys = Vec::new();
+        let mut mouses = Vec::new();
 
         for ev in self.graphics.poll_events() {
             match ev {
@@ -53,7 +54,8 @@ impl<'a> GameManager<'a>{
                     let (x, y) = (x as f32 - w, h - y as f32);
 
                     self.mousepos = (x, y);
-                }
+                },
+                Event::MouseInput(state, button) => mouses.push((state == ElementState::Pressed, button)),
                 _ => ()
             }
         }
@@ -65,6 +67,7 @@ impl<'a> GameManager<'a>{
         let update = FrameInfo{
             delta    : delta,
             keyevents: keys,
+            mouseevents: mouses,
             down_keys: &self.down_keys,
             mousepos : self.mousepos
         };
@@ -80,6 +83,7 @@ pub struct FrameInfo<'a>{
     pub delta: f64,
     /// The last position of the mouse on the screen
     pub mousepos: (f32, f32),
+    mouseevents: Vec<(bool, MouseButton)>,
     keyevents: Vec<(bool, VirtualKeyCode)>,
 
     // All keys that are pressed down
@@ -91,6 +95,11 @@ impl<'a> FrameInfo<'a>{
     /// Returns a slice of all key events that have happened
     pub fn get_key_events(&self) -> &[(bool, VirtualKeyCode)]{
         &self.keyevents
+    }
+    #[inline]
+    /// Returns a slice of all key events that have happened
+    pub fn get_mouse_events(&self) -> &[(bool, MouseButton)]{
+        &self.mouseevents
     }
     #[inline]
     /// Checks whether a key is pressed down
